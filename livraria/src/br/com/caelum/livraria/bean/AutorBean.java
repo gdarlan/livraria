be4def906b56728f25cc727f47a50669afad0e66
gdarlan <gdarlan@DESKTOP-EMG4CI3>
2019-04-06 14:19:28 -0300

    formulario autor capitulo 3

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,11 +1,13 @@
 package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -14,9 +16,13 @@ public class AutorBean {
 		return autor;
 	}
 
-	public void gravar() {
-		System.out.println("Gravando autor " + this.autor.getNome());
-
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+	public void setAutor(Autor autor) {
+		this.autor = autor;
 	}
+
+	public String gravar() {
+		new DAO<Autor>(Autor.class).adiciona(this.getAutor());
+		return "livro?faces-redirect=true";
+	}
+
 }
