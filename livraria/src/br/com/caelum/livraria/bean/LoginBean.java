44e098705a309f669d6131d99e54d845f9d55ab8
gdarlan <gdarlan@DESKTOP-EMG4CI3>
2019-04-07 01:57:51 -0300

    login completo

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -11,7 +12,6 @@ import br.com.caelum.livraria.modelo.Usuario;
 public class LoginBean {
 
 	private Usuario usuario = new Usuario();
-	private UsuarioDao dao = new UsuarioDao();
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -24,6 +24,8 @@ public class LoginBean {
 	public String entrar() {
 		boolean existe=new UsuarioDao().existe(this.usuario);
 		if(existe) {
+			FacesContext context =FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		return null;
